b35f39f33e20818ab6e647062cd2540cf0366b66
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2022-01-01 16:15:18 -0600

    minor changes

--- vm/client/run.sh
+++ vm/client/run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:4200/tcp client
+docker run -d -p 80:80/tcp --name client pabloc54/client
--- vm/server/index.js
+++ vm/server/index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
   return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
 const Registro = require('./models/registro')
 const express = require('express');
 const http = require('http');
@@ -12,108 +11,154 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 const express_1 = __importDefault(require("express"));
 
-// REDIS
+//---------------- CONEXION - REDIS
 const client = redis.createClient({
   url: 'redis://:grupo14so1@34.82.25.144:6379'
 });
-
-client.on('connect', function () {
+client.on('connect', function(){
   console.log("conectado redis");
 });
 
+//---------- CONFIG SERVER
 let app = express()
-
 const servidor = http.createServer(app);
-
 const io = require('socket.io')(servidor, {
   cors: { origin: '*' }
 });
-
 app = express_1.default();
-
-const port = 8080;
+const port = process.env.PORT || 5050;
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
-
-app.use(morgan_1.default('dev'));
+app.use(morgan_1.default('dev')); // debuguer para ver errores
 app.use(cors_1.default()); // para conectarse al cliente
-
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 
-//var MongoClient = require('mongodb').MongoClient;
-//var url = "mongodb://35.230.106.175:27017";
 
+//------------- CONNEXION MONGODB
+var MongoClient = require('mongodb').MongoClient;
+var url_mongodb = "mongodb://www.so1g14.tk:27017";
 
-/*MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("covid");
-  dbo.collection("vacundata").find({}).toArray(function(err, result) {
+var respuesta = ""; // ANSWER FOR EMIT
+var respuesta1 = ""; // ANSWER FOR EMIT
+var respuesta2 = ""; // ANSWER FOR EMIT
+var respuesta3 = ""; // ANSWER FOR EMIT
+
+//-------------- GET MONGODB DATA
+async function getMongoDB(){
+
+  //---------- porcentaje de vacunados, una dosis, por departamentos, etc., en MongoDB
+  MongoClient.connect(url_mongodb, function(err, db) {
     if (err) throw err;
-    console.log(result);
-    io.emit('message', result);
-    db.close();
+    var dbo = db.db("covid");
+    dbo.collection("vacundata").aggregate(
+      [ 
+        {
+          $group : {
+            _id : '$location',
+            count : {$sum : { $cond : [ { $eq : ['$n_dose', 1]}, 1, 0]}}
+          }
+        },
+        { $sort : {count : -1}}
+      ]
+    ).toArray(function(err, result) {
+      if (err) throw err;
+      var s = JSON.stringify(result)
+      respuesta1 = "{ \"mongodb1\" : "+s+"},"
+      db.close();
+    });
   });
-});*/
-<<<<<<< HEAD:server/index.js
-var respuesta = "";
-=======
 
-async function start() {
-  await client.connect()
+  // Top 3 de áreas con mayor vacunados con esquema completo en MongoDB.
+  //---------- porcentaje de vacunados, esquema completo, por departamentos, etc., en MongoDB
+  MongoClient.connect(url_mongodb, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("covid");
+    dbo.collection("vacundata").aggregate(
+      [ 
+        {
+          $group : {
+            _id : '$location',
+            count : {$sum : { $cond : [ { $gt : ['$n_dose', 1]}, 1, 0]}}
+          }
+        },
+        { $sort : {count : -1}}
+      ]
+    ).toArray(function(err, result) {
+      if (err) throw err;
+      //console.log("MONGO DB2 ----------", result);
+      var s = JSON.stringify(result)
+      respuesta2 = "{ \"mongodb2\" : "+s+"}"
+      db.close();
+    });
+  });
+
+  MongoClient.connect(url_mongodb, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("covid");
+    dbo.collection("vacundata").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      //console.log("MONGO DB2 ----------", result);
+      var s = JSON.stringify(result)
+      respuesta3 = "{ \"mongodb3\" : "+s+"}"
+      db.close();
+    });
+  });
+
+  
 }
->>>>>>> 2c718be8b82133760e31ef35895dcd8fde99fa38:vm/server/index.js
 
+
+//------------- GET REDIS DATA
 async function getVacunados() {
   try {
-    await client.connect();
+    try{
+      await client.connect();
+    }
+    catch{}
+    
     let retorno = await client.lRange('list-vacun-data', '0', '4');
     let retorno1 = await client.lLen('ninos');
     let retorno2 = await client.lLen('adolescentes');
     let retorno3 = await client.lLen('jovenes');
     let retorno4 = await client.lLen('adultos');
     let retorno5 = await client.lLen('vejez');
-    var envio = {
-      "retorno": retorno,
-      "retorno1": retorno1,
-      "retorno2": retorno2,
-      "retorno3": retorno3,
-      "retorno4": retorno4,
-      "retorno5": retorno5,
-    }
-    respuesta = "{\"retorno\":["+retorno+"],\"retorno1\":"+retorno1+",\"retorno2\":"+retorno2+",\"retorno3\":"+retorno3+",\"retorno4\":"+retorno4+",\"retorno5\":"+retorno5+"}"
+    //respuesta += "{\"retorno\":["+retorno+"],\"retorno1\":"+retorno1+",\"retorno2\":"+retorno2+",\"retorno3\":"+retorno3+",\"retorno4\":"+retorno4+",\"retorno5\":"+retorno5+"}"
     await client.quit();
-<<<<<<< HEAD:server/index.js
+    var envio = {
+      "retorno" : retorno,
+      "retorno1" : retorno1,
+      "retorno2" : retorno2,
+      "retorno3" : retorno3,
+      "retorno4" : retorno4,
+      "retorno5" : retorno5,
+    }
+    var s = JSON.stringify(envio)
+    respuesta = s
     //await client.disconnect();
-=======
-    await client.disconnect();
-
->>>>>>> 2c718be8b82133760e31ef35895dcd8fde99fa38:vm/server/index.js
   } catch (err) {
-    console.error("ERROR", err);
+    //console.error("ERROR", err);
   }
 }
 
+
+//----------- CONNEXION SOCKET.IO
 io.on('connection', (socket) => {
   console.log("conectado")
-<<<<<<< HEAD:server/index.js
   
   setInterval( () => {
-=======
-
-  setInterval(() => {
-    console.log("message")
->>>>>>> 2c718be8b82133760e31ef35895dcd8fde99fa38:vm/server/index.js
-    getVacunados()
-    console.log("message", respuesta)
+    getVacunados();
+    getMongoDB();
+    //console.log("message", respuesta)
     
-    socket.emit("message", respuesta)
+    socket.emit("message", respuesta + "|\n"+ respuesta1 +"|\n"+respuesta2+"|\n"+respuesta3 )
+    respuesta = ""
   }, 5000);
 });
 
 
 
-
-servidor.listen(port, () => console.log(`listening on port ${port}`))
+//------- LISTENING ON PORT
+servidor.listen(port, () => console.log(`listening on port ${port}`) )
 
 
